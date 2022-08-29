1579dde5b7aae9a612a8c2391175ad680e9f946b
StanchoPanza <stanislav.stoev@gmail.com>
2022-08-29 20:37:47 +0300

    Add Password Guess

---
+++ Programing Basics/Lab Problems/Conditional Statements/04. PasswordGuess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pass = ("s3cr3t!P@ssw0rd");
+            string password = Console.ReadLine();
+            if (pass == password)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
