32df2de388e7dd7a88ae6b3a24aad20f299c4c86
StanchoPanza <stanislav.stoev@gmail.com>
2022-08-31 20:10:46 +0300

    Adding CharacterSequence

---
+++ Programing Basics/Lab Problems/For Loop/05. Character Sequence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CharacterSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                char letter = a[i];
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
