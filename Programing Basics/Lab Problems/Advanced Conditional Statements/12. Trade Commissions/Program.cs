a565a8783145e8e143bffab2012a08744b92e202
StanchoPanza <stanislav.stoev@gmail.com>
2022-08-31 19:59:28 +0300

    Added TradeCommissions

---
+++ Programing Basics/Lab Problems/Advanced Conditional Statements/12. Trade Commissions/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace TradeCommissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commission = 0;
+            if (city == "Sofia")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.12;
+                }
+                else if (sales <= 10000 && sales > 1000)
+                {
+                    commission = 0.08;
+
+                }
+                else if (sales <= 1000 && sales > 500)
+                {
+                    commission = 0.07;
+                }
+                else if (sales <= 500 && sales > 0)
+                {
+                    commission = 0.05;
+                }
+                
+            }
+            if (city == "Varna")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.13;
+                }
+                else if (sales <= 10000 && sales > 1000)
+                {
+                    commission = 0.1;
+
+                }
+                else if (sales <= 1000 && sales > 500)
+                {
+                    commission = 0.075;
+                }
+                else if (sales <= 500 && sales > 0)
+                {
+                    commission = 0.045;
+                }
+                
+            }
+            
+            if (city == "Plovdiv")
+            {
+                if (sales > 10000)
+                {
+                    commission = 0.145;
+                }
+                else if(sales <= 10000 && sales > 1000)
+                {
+                    commission = 0.12;
+
+                }
+                else if(sales <= 1000 && sales > 500)
+                {
+                    commission = 0.08;
+                }
+                else if(sales <= 500 && sales > 0)
+                {
+                    commission = 0.055;
+                }
+            }
+            double total = sales * commission;
+
+            if (commission > 0 || city == "Sofia" && city == "Varna" && city == "Plovdiv")
+            {
+                Console.WriteLine($"{total:F2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+        
+    }
+}
