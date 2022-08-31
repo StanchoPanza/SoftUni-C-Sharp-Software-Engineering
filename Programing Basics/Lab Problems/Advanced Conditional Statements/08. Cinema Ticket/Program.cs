055c050220ca5930b4c42db19e5bd70ceb1f12a8
StanchoPanza <stanislav.stoev@gmail.com>
2022-08-31 19:55:13 +0300

    Adding CinemaTicket

---
+++ Programing Basics/Lab Problems/Advanced Conditional Statements/08. Cinema Ticket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CinemaTicket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dayOfWeek = Console.ReadLine();
+            if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday" || dayOfWeek == "Friday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (dayOfWeek == "Wednesday" || dayOfWeek == "Thursday")
+            {
+                Console.WriteLine(14);
+            }
+            else
+            {
+                Console.WriteLine(16);
+            }
+        }
+    }
+}
