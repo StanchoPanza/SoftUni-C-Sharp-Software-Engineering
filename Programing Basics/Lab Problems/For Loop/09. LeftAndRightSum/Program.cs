a458e2b708b87e40f7f48166e8c16f11fe9432f1
StanchoPanza <stanislav.stoev@gmail.com>
2022-09-21 20:51:33 +0300

    adding LeftAndRightSum

---
+++ Programing Basics/Lab Problems/For Loop/09. LeftAndRightSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumLeft = 0;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sumLeft += number;
+            }
+            
+
+            int sumRight = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sumRight += number;
+            }
+            if (sumLeft == sumRight)
+            {
+                Console.WriteLine($"Yes, sum = {sumLeft}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs (sumRight - sumLeft)}");
+            }
+        }
+    }
+}
