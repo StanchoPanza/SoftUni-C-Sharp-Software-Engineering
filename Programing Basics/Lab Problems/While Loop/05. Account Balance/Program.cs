42a2790fd4d302620e7b7a5ef1e7e7e3b56e3628
StanchoPanza <stanislav.stoev@gmail.com>
2022-09-25 12:43:08 +0300

    adding AccountBalance

---
+++ Programing Basics/Lab Problems/While Loop/05. Account Balance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            double sum = 0;
+
+            while (input != "NoMoreMoney")
+            {
+
+                double num = double.Parse(input);
+                
+
+                
+
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum += num;
+
+                Console.WriteLine($"Increase: {num:F2}");
+                input = (Console.ReadLine());
+
+            }
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
