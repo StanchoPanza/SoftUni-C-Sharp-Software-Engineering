6216b9ce629069c88392ecf1a90c6a0831381c4d
StanchoPanza <stanislav.stoev@gmail.com>
2022-10-23 09:21:14 +0300

    Adding Combinations

---
+++ Programing Basics/Lab Problems/Nested Loops/03. Combinations/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int validCombinatios = 0;
+
+
+            for (int i = 0; i <= n; i++)
+            {
+                
+                for (int j = 0; j <= n; j++)
+                {
+                    
+                    for (int k = 0; k <= n; k++)
+                    {
+                        
+                        if (i + j + k == n)
+                        {
+                            validCombinatios++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(validCombinatios);
+        }
+    }
+}
