0d7b3ce6b2257c6b0a5ffa03f872f7cba32b1603
StanchoPanza <stanislav.stoev@gmail.com>
2022-10-23 09:08:43 +0300

    Adding MinNumber

---
+++ Programing Basics/Lab Problems/While Loop/07. Min Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+
+            int minNum = int.MaxValue;
+
+            while (num != "Stop")
+            {
+                int input = int.Parse(num);
+
+                if (input < minNum)
+                {
+                    minNum = input;
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
