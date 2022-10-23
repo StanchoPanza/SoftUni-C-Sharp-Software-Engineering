324df6862b709ac2fde86bfc3f479fa3a28e0d91
StanchoPanza <stanislav.stoev@gmail.com>
2022-10-23 09:06:49 +0300

    Adding MaxNumber

---
+++ Programing Basics/Lab Problems/While Loop/06. MaxNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+
+            int maxNum = int.MinValue;
+
+            while (num != "Stop")
+            {
+                int input = int.Parse(num);
+
+                if (input > maxNum)
+                {
+                    maxNum = input;
+                }
+                num = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
+        }
+    }
+}
