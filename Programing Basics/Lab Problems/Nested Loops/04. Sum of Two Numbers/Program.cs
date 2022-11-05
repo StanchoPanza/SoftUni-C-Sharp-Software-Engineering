650b800e5df87ede35dc77a746b155f053766ae5
StanchoPanza <stanislav.stoev@gmail.com>
2022-11-05 09:05:29 +0200

    Adding SumofTwoNumbers

---
+++ Programing Basics/Lab Problems/Nested Loops/04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SumofTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int combinationcount = 0;
+            int sum = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                for (int j = a; j <= b; j++)
+                {
+                    combinationcount++;
+                    sum = i + j;
+
+                    if (sum == c)
+                    {
+                        Console.WriteLine($"Combination N:{combinationcount} ({i} + {j} = {sum})");
+                        return;
+                    }
+                }                          
+            }
+            Console.WriteLine($"{combinationcount} combinations - neither equals {c}");
+        }
+    }
+}
