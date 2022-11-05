ffef5b368f7edd2607247268f9b0809db7cc5691
StanchoPanza <stanislav.stoev@gmail.com>
2022-11-05 09:16:20 +0200

    Adding Travelling

---
+++ Programing Basics/Lab Problems/Nested Loops/05. Travelling/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+            
+
+            while (country != "End")
+            {
+                double budget = double.Parse(Console.ReadLine());
+                double availableSum = 0;
+
+                while (availableSum < budget)
+                {                    
+                    availableSum += double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Going to {country}!");
+                country = Console.ReadLine();
+            }
+        }
+    }
+}
