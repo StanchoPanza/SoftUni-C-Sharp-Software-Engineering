dd679d909057ca36511555409fdc00da6a83bf91
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-13 18:27:30 +0200

    Adding VacationBookList

---
+++ Programing Basics/Exercise/04. Vacation Books List/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int numberOfDays = int.Parse(Console.ReadLine());
+
+            double totalHours = (numberOfPages / pagesPerHour);
+            double totalDays = (totalHours / numberOfDays);
+            Console.WriteLine(totalDays);
+
+
+        }
+    }
+}
