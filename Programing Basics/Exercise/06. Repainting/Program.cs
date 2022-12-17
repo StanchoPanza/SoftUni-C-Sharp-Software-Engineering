ed5be72ef1582d8de94f4d0e5515a492c1d07209
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-17 18:32:15 +0200

    Adding Repainting

---
+++ Programing Basics/Exercise/06. Repainting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Repainting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double safetyNylon = double.Parse(Console.ReadLine()) + 2;
+            double nylonPrice = safetyNylon * 1.5;
+            double paint = double.Parse(Console.ReadLine()) * 1.1;
+            double paintPrice = paint * 14.5;
+            double paintThinner = double.Parse(Console.ReadLine()) * 5.00;
+            double hours = double.Parse(Console.ReadLine());
+            double priceMaterials = nylonPrice + paintPrice + paintThinner + 0.4;
+            double salary = (priceMaterials * 0.3) * hours;
+            double totalPrice = (priceMaterials + salary);
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
