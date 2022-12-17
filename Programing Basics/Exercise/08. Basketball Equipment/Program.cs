e1d22a7955d33afd5be38f4f3f26e2c4d449eab7
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-17 18:34:54 +0200

    Adding Basketball

---
+++ Programing Basics/Exercise/08. Basketball Equipment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Baketball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taxPerYear = int.Parse(Console.ReadLine());
+            double trainers = taxPerYear - (taxPerYear * 0.4);
+            double outfit = trainers - (trainers * 0.2);
+            double ball = outfit / 4;
+            double acc = ball / 5;
+            double expenses = taxPerYear + trainers + outfit + ball + acc;
+            Console.WriteLine(expenses);
+        }
+    }
+}
