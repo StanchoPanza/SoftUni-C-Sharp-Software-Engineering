a9c06d4276076529ba6bcbe9fc2a1a08b43c57f3
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-17 18:29:40 +0200

    Adding SuppliesforSchool

---
+++ Programing Basics/Exercise/05. Supplies for School/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuppliesforSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chemicals = double.Parse(Console.ReadLine()) * 5.80;
+            double markers = double.Parse(Console.ReadLine()) * 7.20;
+            double washliquid = double.Parse(Console.ReadLine()) * 1.20;
+            double percentReduction = double.Parse(Console.ReadLine()) / 100;
+            double totalPrice = chemicals + markers + washliquid;
+            double discount = totalPrice * percentReduction;
+            totalPrice -= discount;
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
