065bc6e001f8f7e1e0742a4de350b6d5958297fa
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-17 18:35:44 +0200

    Adding FishTank

---
+++ Programing Basics/Exercise/09. Fish Tank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int chicken = int.Parse(Console.ReadLine());
+            int fish = int.Parse(Console.ReadLine());
+            int veggie = int.Parse(Console.ReadLine());
+
+            double foodsum = chicken * 10.35 + fish * 12.40 + veggie * 8.15;
+            Console.WriteLine(foodsum);
+            double withDesert = (foodsum * 1.2) + 2.5;
+            Console.WriteLine(withDesert);
+
+        }
+    }
+}
