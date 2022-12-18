4382990825a8ca843dfa9d15809fbbf1d2377a79
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-18 12:29:34 +0200

    Adding GodzillaVsKong

---
+++ Programing Basics/Exercise/Conditional Statements/05. Godzilla vs. Kong/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GodzillaVsKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shootingTime = double.Parse(Console.ReadLine());
+            double scenesCount = double.Parse(Console.ReadLine());
+            double timePerScene = double.Parse(Console.ReadLine());
+
+            double fieldPreparationTime = shootingTime * 0.15;
+
+            double SceneShootingTime = scenesCount * timePerScene;
+
+            double totalShootingTime = SceneShootingTime + fieldPreparationTime;
+
+            double timeLeft = Math.Abs(shootingTime - totalShootingTime);
+
+            if (totalShootingTime <= shootingTime)
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeLeft)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(timeLeft)} minutes.");
+            }
+        }
+    }
+}
