76aaefcd120840035fc7b8bc7494462ade5c527f
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-18 12:28:02 +0200

    Adding TimePlus15Minutes

---
+++ Programing Basics/Exercise/Conditional Statements/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TimePlus15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            if (minutes < 45)
+            {
+                minutes += 15;
+            }
+            else if (minutes >= 45 && hours !=23)
+            {
+                hours += 1;
+                minutes = minutes + 15 - 60;
+            }
+            else if (minutes >= 45 && hours == 23)
+            {
+                minutes = minutes + 15 - 60;
+                hours = 00;
+            }
+            {
+                Console.WriteLine($"{hours}:{minutes:D2}"); 
+
+            }
+        }   
+    }
+}
