8e5b230b597881ae34e776251afcc37a8d2fe04e
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-19 19:01:27 +0200

    Adding FishingBoat

---
+++ Programing Basics/Exercise/Advanced Conditional Statements/04. Fishing Boat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FishingBoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            int fishermanCount = int.Parse(Console.ReadLine());
+
+            double rent = 0.00;
+
+            switch (season)
+            {
+                case "Spring":
+                    rent = 3000.00;
+                    break;
+                case "Summer":
+                case "Autumn":
+                    rent = 4200.00;
+                    break;
+                case "Winter":
+                    rent = 2600.00; 
+                        break;
+            }
+            if (fishermanCount <= 6)
+            {
+                rent -= rent * 0.10;
+            }
+            else if (fishermanCount <= 11)
+            {
+                rent -= rent * 0.15;
+            }
+            else
+            {
+                rent -= rent * 0.25;
+            }
+            if (fishermanCount % 2 == 0 && season != "Autumn")
+            {
+                rent -= rent * 0.05;
+            }
+            if (budget >= rent)
+            {
+                Console.WriteLine($"Yes! You have {budget - rent:F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs (budget - rent):F2} leva.");
+            }
+        }
+    }
+}
