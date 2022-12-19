64338f20cd78796f84df83cb199356ccebd1c552
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-19 19:02:38 +0200

    Adding Journey

---
+++ Programing Basics/Exercise/Advanced Conditional Statements/05. Journey/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+
+            double rent = 0.00;
+            string destination = "";
+            string place = "";
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if (season == "summer")
+                {
+                    place = "Camp";
+                    rent = budget * 0.30;
+                }
+                else
+                {
+                    place = "Hotel";
+                    rent = budget * 0.70;
+                }
+            }
+            else if (budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    place = "Camp";
+                    rent = budget * 0.40;
+                }
+                else
+                {
+                    place = "Hotel";
+                    rent = budget * 0.80;
+                }
+            }
+            else
+            {
+                destination = "Europe";
+                rent = budget * 0.90;
+                place = "Hotel";
+            }
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{place} - {rent:F2}");
+        }
+    }
+}
