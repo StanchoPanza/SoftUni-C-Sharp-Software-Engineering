66e5b20ce6be783ef532a2f417789c8700e10a15
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-19 19:00:38 +0200

    Adding NewHouse

---
+++ Programing Basics/Exercise/Advanced Conditional Statements/03. New House/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace NewHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string flowers = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            double budget = double.Parse(Console.ReadLine());
+
+            double price = 0.00;
+
+            if (flowers == "Roses")
+            {
+                price = count * 5.00;
+
+                if (count > 80)
+                {
+                    price = price - price * 0.10;
+                }
+            }
+            else if (flowers == "Dahlias")
+            {
+                price = count * 3.80;
+                if (count > 90)
+                {
+                    price = price - price * 0.15;
+                }
+            }
+            else if (flowers == "Tulips")
+            {
+                price = count * 2.80;
+                if (count > 80)
+                {
+                    price = price - price * 0.15;
+                }
+                
+
+            }
+            else if (flowers == "Narcissus")
+            {
+                price = count * 3.00;
+                if (count < 120)
+                {
+                    price = price + price * 0.15;
+                }
+            }
+            else
+            {
+                price = count * 2.50;
+                if (count < 80)
+                {
+                    price = price + price * 0.20;
+                }
+            }
+            if (price <= budget)
+            {
+                Console.WriteLine($"Hey, you have a great garden with {count} {flowers} and {Math.Abs(price - budget):F2} leva left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money, you need {price - budget:F2} leva more.");
+            }
+        }
+    }
+}
