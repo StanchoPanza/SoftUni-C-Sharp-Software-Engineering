7f3c4b19abfff54b5535bebd7542c7ab3db16488
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-19 18:54:06 +0200

    Adding Cinema

---
+++ Programing Basics/Exercise/Advanced Conditional Statements/01. Cinema/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+            int seats = rows * colums;
+            double ticketPrice = 0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    ticketPrice = 12;
+
+                     break;
+                case "Normal":
+                    ticketPrice = 7.50;
+                    break;
+                default:
+                    ticketPrice = 5.00;
+                    break;
+            }
+            double totalPrice = seats * ticketPrice;
+
+            Console.WriteLine($"{totalPrice:F2} leva");
+        }
+    }
+}
