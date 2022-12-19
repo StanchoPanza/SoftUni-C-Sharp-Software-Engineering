ca3f3c36407bad56768b01e1e8409fd252841561
StanchoPanza <stanislav.stoev@gmail.com>
2022-12-19 18:54:53 +0200

    Adding SummerOutfit

---
+++ Programing Basics/Exercise/Advanced Conditional Statements/02. Summer Outfit/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SummerOutfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int degrees = int.Parse(Console.ReadLine());
+            string timeOfDay = Console.ReadLine();
+            string outfit = "a";
+            string shoes = "b";
+            if (timeOfDay == "Morning")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+
+                }
+                else if (degrees <= 24)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+                else if (degrees > 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+            }
+            else if (timeOfDay == "Afternoon")
+            {
+                if (degrees >= 10 && degrees <= 18)
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+
+                }
+                else if (degrees <= 24)
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+                else if (degrees > 24)
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+            }
+            else
+            {
+                outfit = "Shirt";
+                shoes = "Moccasins";
+            }
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+        }
+    }
+}
