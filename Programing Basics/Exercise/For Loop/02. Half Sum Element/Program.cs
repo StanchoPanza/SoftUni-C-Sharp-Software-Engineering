505d542f870c8742c184a988573c21c25d28cf67
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-23 20:43:51 +0200

    Adding HalfSumElement

---
+++ Programing Basics/Exercise/For Loop/02. Half Sum Element/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double sum = 0;
+            double max = int.MinValue;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            if (max == sum / 2)
+            {
+                Console.WriteLine($"Yes\nSum = {max}");
+            }
+            else
+            {
+                double diff = sum -= max;
+                Console.WriteLine($"No\nDiff = {Math.Abs (diff - max)}");
+            }
+           
+        }
+    }
+}
