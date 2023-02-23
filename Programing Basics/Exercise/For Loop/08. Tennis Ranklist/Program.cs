2a4204761f1d827f9d171705703abfef4e4565f2
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-23 20:48:47 +0200

    Adding TennisRanklist

---
+++ Programing Basics/Exercise/For Loop/08. Tennis Ranklist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace TennisRanklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tournaments = int.Parse(Console.ReadLine());
+            int points = int.Parse(Console.ReadLine());
+
+            int totalPoint = 0;
+            double tournamentsWon = 0;
+
+            for (int i = 0; i < tournaments; i++)
+            {
+                string position = Console.ReadLine();
+                if (position == "W")
+                {
+                    totalPoint += 2000;
+                    tournamentsWon++;
+                }
+                else if (position == "F")
+                {
+                    totalPoint += 1200;
+                }
+                else if (position == "SF")
+                {
+                    totalPoint += 720;
+                }
+            }
+            double avaragePoints = totalPoint / tournaments;
+
+            Console.WriteLine($"Final points: {totalPoint + points}");
+            Console.WriteLine($"Average points: {avaragePoints}");
+            Console.WriteLine($"{tournamentsWon / tournaments * 100:F2}%");
+
+        }
+    }
+}
