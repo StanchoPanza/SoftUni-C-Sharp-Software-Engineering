52553822fea5d0a1c1dd0b534d2f250461c79a2c
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-23 20:46:21 +0200

    Adding Salary

---
+++ Programing Basics/Exercise/For Loop/05. Salary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                string webPage = Console.ReadLine();
+                switch (webPage)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;                    
+                }
+                if (salary <= 0)
+                {
+                    break;
+                }
+
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+            
+        }
+    }
+}
