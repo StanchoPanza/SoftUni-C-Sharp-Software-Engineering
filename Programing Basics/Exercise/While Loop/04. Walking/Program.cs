be8965cfe58d0458a8bf9a190565031b7ee531e8
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-24 20:03:47 +0200

    Adding Walking

---
+++ Programing Basics/Exercise/While Loop/04. Walking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int totalSteps = 0;
+            int stepsToHome = 0;
+
+            while (totalSteps < 10000)
+            {
+                string steps = Console.ReadLine();
+                
+                if (steps == "Going home")
+                {
+                    stepsToHome = int.Parse(Console.ReadLine());
+                    break;
+                }
+
+                int input = int.Parse(steps);
+                totalSteps += input;
+
+                if (totalSteps >= 10000)
+                {
+                    break;
+                }
+            }
+
+            if (totalSteps + stepsToHome >= 10000)
+            {
+                Console.WriteLine($"Goal reached! Good job!\n{totalSteps + stepsToHome - 10000} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - totalSteps - stepsToHome} more steps to reach goal.");
+            }
+
+        }
+    }
+}
