b49dbcd6ab4ab2c27faa58698bbcf0c2bb243891
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-24 20:09:15 +0200

    Add NumberPyramid

---
+++ Programing Basics/Exercise/Nested Loops/01. Number Pyramid/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int currentNum = 1;
+
+            bool isBigger = false;
+
+            for (int i = 1; i <= num; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (currentNum > num)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(currentNum + " ");
+                    currentNum++;
+
+
+                }
+                if (isBigger)
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
