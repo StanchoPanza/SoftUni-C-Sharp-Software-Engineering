56e071392fa641e9deec24bc47233101ed23dafc
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-24 20:02:14 +0200

    Adding ExamPreparation

---
+++ Programing Basics/Exercise/While Loop/02. Exam Preparation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int poorGrades = int.Parse(Console.ReadLine());
+
+            int lowGradeCount = 0;
+            int highGradeCount = 0;
+            double gradeSum = 0;
+            string lastTask = "";
+            bool failed = true;
+
+
+            while (lowGradeCount < poorGrades)
+            {
+                string taskName = Console.ReadLine();
+                if (taskName == "Enough")
+                {
+                    failed = false;
+                    break;
+                }
+                int grade = int.Parse(Console.ReadLine());
+                if (grade <= 4)
+                {
+                    lowGradeCount++;
+                }
+                gradeSum += grade;
+                highGradeCount++;
+                lastTask = taskName;
+
+            }
+            if (failed)
+            {
+                Console.WriteLine($"You need a break, {poorGrades} poor grades.");
+            }
+            else
+            {
+                Console.WriteLine($"Average score: {gradeSum / highGradeCount:F2}");
+                Console.WriteLine($"Number of problems: {highGradeCount}");
+                Console.WriteLine($"Last problem: {lastTask}");
+            }
+
+        }
+    }
+}
