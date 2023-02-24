68da639fe6d70566a570a7886127f7202bb29961
StanchoPanza <stanislav.stoev@gmail.com>
2023-02-24 20:01:08 +0200

    Adding OldBooks

---
+++ Programing Basics/Exercise/While Loop/01. Old Books/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FavoriteBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string favoriteBook = Console.ReadLine();
+                int counter = 0;
+                string nextBookName = Console.ReadLine();
+
+                while (nextBookName != "No More Books")
+                {
+                    if (nextBookName == favoriteBook)
+                    {
+                        break;
+                    }
+                    counter++;
+                    nextBookName = Console.ReadLine();
+                }
+                if (nextBookName == favoriteBook)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                }
+                else
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {counter} books.");
+                }
+            }
+        }
+    }
+}
