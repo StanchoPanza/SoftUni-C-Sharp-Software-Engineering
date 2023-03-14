f4aaf55320a6fa05d904a72075e392f23f3f6dea
StanchoPanza <stanislav.stoev@gmail.com>
2023-03-14 19:13:47 +0200

    Add ForeignLanguages

---
+++ Programing Fundamentals/Lab Problems/Basic Syntax, Conditional Statements and Loops/06. Foreign Languages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ForeignLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string country = Console.ReadLine();
+
+            string laguage = String.Empty;
+
+            if (country == "England" || country == "USA")
+            {
+                laguage = "English";
+            }
+            else if (country == "Spain" || country == "Argentina" || country == "Mexico")
+            {
+                laguage = "Spanish";
+            }
+            else
+            {
+                laguage = "unknown";
+            }
+            Console.WriteLine(laguage);
+        }
+    }
+}
