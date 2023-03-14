92df925b89b274955b55895e4a7f25dea88f6a52
StanchoPanza <stanislav.stoev@gmail.com>
2023-03-14 19:19:14 +0200

    Add MultiplicationTable2

---
+++ Programing Fundamentals/Lab Problems/Basic Syntax, Conditional Statements and Loops/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MultiplicationTable2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int multiplicator = int.Parse(Console.ReadLine());
+
+
+            if (multiplicator > 10)
+            {
+                Console.WriteLine($"{num} X {multiplicator} = {num * multiplicator}");
+                return;
+            }
+            else
+            {
+                for (int i = multiplicator; i <= 10; i++)
+                {
+                    int result = num * i;
+                    Console.WriteLine($"{num} X {i} = {result}");
+                }
+            }
+
+            
+        }
+    }
+}
