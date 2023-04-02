7fd862235b45b169d30329575c28e4586f094104
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 12:42:15 +0300

    Add RefactorVolumeofPyramid

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RefactorVolumeofPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = 0;
+
+            double widht = 0;
+
+            double heigth  = 0;
+
+            Console.Write("Length: ");
+            length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            widht = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            heigth = double.Parse(Console.ReadLine());
+
+            double baseArea = length * widht;
+            double result = (baseArea * heigth) / 3;
+            Console.Write($"Pyramid Volume: {result:f2}");
+
+        }
+    }
+}
