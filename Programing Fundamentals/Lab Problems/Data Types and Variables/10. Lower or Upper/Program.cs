00bff5c32b6718d73bfae1d3e185354d393bd5eb
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 12:22:33 +0300

    Add LowerOrUpper

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/10. Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+
+            if (Char.IsLower(a))
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
