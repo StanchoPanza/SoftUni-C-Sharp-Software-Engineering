2f75a36024e9f8e44bb41fc18b3f6f77c5fe517b
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 13:33:36 +0300

    Add SumEvenNumbers

---
+++ Programing Fundamentals/Lab Problems/Arrays/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿    using System;
+    using System.Linq;
+
+    namespace SumEvenNumbers
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                int[] num = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                int sum = 0;
+
+                foreach (int i in num)
+                {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
