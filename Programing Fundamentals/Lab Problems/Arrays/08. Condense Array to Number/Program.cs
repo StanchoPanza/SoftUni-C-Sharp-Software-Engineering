d4451c5d5aba55b2ac6911db2236eff2105295e2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 13:35:25 +0300

    Add CondenseArraytoNumber

---
+++ Programing Fundamentals/Lab Problems/Arrays/08. Condense Array to Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace CondenseArraytoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int count = numArray.Length;
+
+            while (numArray.Length > 1)
+            {
+                int[] sumArrey = new int[numArray.Length - 1];
+                for (int i = 0; i < numArray.Length - 1; i++)
+                {
+                    sumArrey[i] = numArray[i] + numArray[i + 1];
+                }
+                numArray = sumArrey;
+            }
+
+            Console.WriteLine(numArray[0]);
+
+        }
+    }
+}
