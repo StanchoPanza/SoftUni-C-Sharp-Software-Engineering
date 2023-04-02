dca445a85aae3db3055bdbe7b20807358c877134
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 13:34:12 +0300

    Add EvenAndOddSubtraction

---
+++ Programing Fundamentals/Lab Problems/Arrays/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int sumOdd = 0;
+            int sumEven = 0;
+            int result = 0;
+
+            foreach (int num in nums)
+            {
+                if (num % 2 == 0)
+                {
+                    sumEven += num;
+                }
+                else
+                {
+                    sumOdd += num;
+                }
+                result = sumEven - sumOdd;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
