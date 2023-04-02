1a0b09f0174a5b3c649b728245f0fdd46894c985
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 13:33:01 +0300

    Add ReverseArrayOfString

---
+++ Programing Fundamentals/Lab Problems/Arrays/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseArrayOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = (Console.ReadLine().Split());
+
+            for (int i = input.Length -1; i >=0 ; i--)
+            {
+                Console.Write($"{input[i]} ");
+            }
+        }
+    }
+}
