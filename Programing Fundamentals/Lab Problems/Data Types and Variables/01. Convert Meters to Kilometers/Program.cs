83b7df659585fcad95e612a560c934f9ec47d270
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 11:13:06 +0300

    Add ConvertMeterstoKilometers

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace ConvertMeterstoKilometers
+{
+    internal class Program
+    {
+        public static BigInteger BigInteger { get; private set; }
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                if (Int32.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if (double.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (bool.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else if (Char.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                input = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
