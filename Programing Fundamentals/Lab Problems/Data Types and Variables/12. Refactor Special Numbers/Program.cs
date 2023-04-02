74d77433bc73cbb93b9441b65874901a09591bd2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 12:45:42 +0300

    Add RefactorSpecialNumbers

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int temp = 0;
+            bool result = false;
+            for (int ch = 1; ch <= input; ch++)
+            {
+                temp = ch;
+                while (ch > 0)
+                {
+                    sum += ch % 10;
+                    ch = ch / 10;
+                }
+                result = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", temp, result);
+                sum = 0;
+                ch = temp;
+            }
+
+        }
+    }
+}
