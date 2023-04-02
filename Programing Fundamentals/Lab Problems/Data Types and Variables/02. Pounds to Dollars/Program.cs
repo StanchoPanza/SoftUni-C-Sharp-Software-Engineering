1df4ab83044d6b6fe9f736f1ffaeb857c83b5120
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 11:17:13 +0300

    Add 02.PoundstoDollars

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/02. Pounds to Dollars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConvertMeterstoKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pounds = decimal.Parse(Console.ReadLine());
+
+            decimal dollars = pounds * (decimal)1.31;
+
+            Console.WriteLine($"{dollars:F3}");
+        }
+    }
+}
