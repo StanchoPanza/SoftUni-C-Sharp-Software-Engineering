1e3d090b5d5e1549cb0cdb65ac1a7cf1fa2661a9
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 11:47:32 +0300

    Add SpecialNumbers

---
+++ Programing Fundamentals/Lab Problems/Data Types and Variables/05. Special Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int temp = 0;
+
+            int sum = 0;
+
+            bool result = false;
+
+            for (int i = 1; i <= input; i++)
+            {
+                int num = i%10;
+
+                temp = i;
+
+                while (temp != 0)
+                {
+                    sum += (temp % 10);
+                    temp /= 10;
+                }
+
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    result = true;
+                }
+                else
+                {
+                    result = false;
+                }
+                sum = 0;
+
+                Console.WriteLine(i+" -> "+result);
+            }
+        }
+    }
+}
