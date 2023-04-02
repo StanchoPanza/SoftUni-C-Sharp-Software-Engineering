6afe2fd0fd5c60a4c5955c58ff1979c7063eeb07
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-02 13:32:05 +0300

    Add RoundingNumbers

---
+++ Programing Fundamentals/Lab Problems/Arrays/03. Rounding Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            int[] roundednumbers = new int[nums.Length];
+
+            
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                roundednumbers[i] = (int)Math.Round(nums[i], MidpointRounding.AwayFromZero);
+
+                Console.WriteLine($"{nums[i]} => {string.Join(' ', roundednumbers[i])}");
+            }
+            
+
+        }
+    }
+}
