ddcc733e79f848d463f29ae8535c89abea50d36c
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:20:21 +0300

    Add GreaterofTwoValues

---
+++ Programing Fundamentals/Lab Problems/Methods/09. Greater of Two Values/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GreaterofTwoValues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            var input1 = Console.ReadLine();
+            var input2 = Console.ReadLine();
+
+            if (type == "int")
+            {
+                var a = int.Parse(input1);
+                var b = int.Parse(input2);
+                Console.WriteLine(GetMax(a, b));
+            }
+            else if (type == "char")
+            {
+                var a = char.Parse(input1);
+                var b = char.Parse(input2);
+                Console.WriteLine(GetMax(a, b));
+            }
+            else
+            {
+                Console.WriteLine(GetMax(input1, input2));
+            }
+
+        }
+
+        public static string GetMax(int a, int b)
+        {
+            if (a > b)
+            {
+                return a.ToString();
+            }
+            return b.ToString();
+        }
+
+        public static string GetMax(char a, char b)
+        {
+            if (a > b)
+            {
+                return a.ToString();
+            }
+            return b.ToString();
+        }
+
+        public static string GetMax(string a, string b)
+        {
+            if (a.CompareTo(b) == 1)
+            {
+                return a;
+            }
+            return b;
+        }
+    }
+}
