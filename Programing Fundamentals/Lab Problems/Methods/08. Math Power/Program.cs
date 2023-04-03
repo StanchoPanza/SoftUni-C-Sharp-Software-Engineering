7099ef82949bbc81568a8ec286afecdc5d340787
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:19:01 +0300

    Add MathPower

---
+++ Programing Fundamentals/Lab Problems/Methods/08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MathPower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(MathPower(baseNum, power));
+        }
+        static double MathPower(double a, double b)
+        {
+            double result = 0;
+            result = Math.Pow(a, b);
+            return result;
+        }
+    }
+}
