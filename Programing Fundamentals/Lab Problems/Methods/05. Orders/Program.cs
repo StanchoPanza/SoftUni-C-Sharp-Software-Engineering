0b9f5f98fc72fc0cde0ffb5d70e879ad4ea16ed5
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:16:38 +0300

    Add Orders

---
+++ Programing Fundamentals/Lab Problems/Methods/05. Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int orders = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            
+
+            for (int i = 1; i <= orders; i++)
+            {
+                double pricePerCapsule = double.Parse(Console.ReadLine());
+                double daysInMonth = double.Parse(Console.ReadLine());
+                double capsulesCount = double.Parse(Console.ReadLine());
+
+                double coffiePrice = (daysInMonth * capsulesCount) * pricePerCapsule;
+
+                totalPrice += coffiePrice;
+
+                Console.WriteLine($"The price for the coffee is: ${coffiePrice:F2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
