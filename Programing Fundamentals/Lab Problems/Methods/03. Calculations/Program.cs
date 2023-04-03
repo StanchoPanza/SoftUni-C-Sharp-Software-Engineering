a59725311422bd56dbd5637c05a8bf2d19475dd6
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:14:57 +0300

    Add Calculations

---
+++ Programing Fundamentals/Lab Problems/Methods/03. Calculations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (operation == "add")
+            {
+                Add(num1, num2);
+            }
+            else if (operation == "multiply")
+            {
+                Multiply(num1, num2);
+            }
+            else if (operation == "subtract")
+            {
+                Subtract(num1, num2);
+            }
+            else
+            {
+                Divide(num1, num2);
+            }
+        }
+        static void Add(double a, double b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Multiply(double a, double b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static void Subtract(double a, double b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void Divide(double a, double b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+}
