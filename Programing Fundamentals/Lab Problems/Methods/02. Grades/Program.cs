8744b9b097e529a678647436fae9532b5508f5e2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:14:06 +0300

    Add Grades

---
+++ Programing Fundamentals/Lab Problems/Methods/02. Grades/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeOfGrade(double.Parse(Console.ReadLine()));
+        }
+        static void TypeOfGrade(double grade)
+        {
+            if (grade >= 2 && grade < 3)
+            {
+                Console.WriteLine("Fail");
+            }
+            if (grade >= 3 && grade < 3.5)
+            {
+                Console.WriteLine("Poor");
+            }
+            if (grade >= 3.5 && grade < 4.5)
+            {
+                Console.WriteLine("Good");
+            }
+            if (grade >= 4.5 && grade < 5.5)
+            {
+                Console.WriteLine("Very good");
+            }
+            if (grade >= 5.5 && grade <= 6)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
