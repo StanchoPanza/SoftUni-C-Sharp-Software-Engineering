d0a5360e785d077472e12d34edfdd17478f6b5a4
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:12:20 +0300

    Add SignofIntegerNumbers

---
+++ Programing Fundamentals/Lab Problems/Methods/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SignofIntegerNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(PrintSign(number));
+        }
+        static string PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                return $"The number {number} is positive.";
+            }
+            else if (number < 0)
+            {
+                return $"The number {number} is negative.";
+            }
+                
+            return $"The number {number} is zero.";            
+        }
+    }
+}
