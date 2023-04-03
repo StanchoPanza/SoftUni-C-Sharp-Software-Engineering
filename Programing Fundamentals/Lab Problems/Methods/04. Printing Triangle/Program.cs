661609384935c1818b4ef358b2e969a25882b5ee
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:15:52 +0300

    Add PrintingTriangle

---
+++ Programing Fundamentals/Lab Problems/Methods/04. Printing Triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            PrintTriangle(num);
+        }
+        static void Printline(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+        static void PrintTriangle(int n)
+        {
+            for (int line = 1; line <= n; line++)
+            {
+                Printline(1, line);
+            }
+            for (int line = n - 1; line >= 1; line--)
+            {
+                Printline(1, line);
+            }
+
+        }
+    }
+}
