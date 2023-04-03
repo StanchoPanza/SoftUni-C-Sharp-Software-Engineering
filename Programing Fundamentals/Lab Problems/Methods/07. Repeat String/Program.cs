10804d9ee2f9d2ee187c2745d56c4d5db37d4694
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-03 18:18:07 +0300

    Add RepeatString

---
+++ Programing Fundamentals/Lab Problems/Methods/07. Repeat String/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RepeatString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int num = int.Parse(Console.ReadLine());
+
+            RepeatString(input, num);
+        }
+        static void RepeatString(string input, int a)
+        {
+            for (int i = 1; i <= a; i++)
+            {
+                Console.Write(input);
+            }
+        }
+    }
+}
