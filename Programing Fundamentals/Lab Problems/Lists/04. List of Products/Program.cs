f0342cfdb431af2e1d19805c34f9e22934d2c358
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-05 18:37:51 +0300

    Add ListofProducts

---
+++ Programing Fundamentals/Lab Problems/Lists/04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListofProducts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfProducts = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+
+            for (int i = 0; i < numOfProducts; i++)
+            {
+                string currentProduct = Console.ReadLine();
+                products.Add(currentProduct);
+            }
+            products.Sort();
+
+            for (int i = 0; i < numOfProducts; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
