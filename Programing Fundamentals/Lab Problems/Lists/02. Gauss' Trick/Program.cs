0961f593485f25a70e2c16a6d70a7f413a573463
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-05 18:34:26 +0300

    Add Gauss'Trick

---
+++ Programing Fundamentals/Lab Problems/Lists/02. Gauss' Trick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gauss_Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int lastIndex = numbers.Count - 1;
+
+            for (int i = 0; i < lastIndex; i++)
+            {               
+
+                numbers[i] += numbers[lastIndex];
+                numbers.RemoveAt(lastIndex);
+                lastIndex--;
+            }
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+}
