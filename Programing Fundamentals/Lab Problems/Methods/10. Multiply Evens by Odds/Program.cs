12bbcb5a5896605a3c028f7487f344f7dbd19d99
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-05 18:23:49 +0300

    Add MultiplyEvensbyOdds

---
+++ Programing Fundamentals/Lab Problems/Methods/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int evenSum = GetSumOfEvenDigits(input);
+            int oddSum = GetSumOfOddDigits(input);
+
+            Console.WriteLine(GetMultipleOfEvenAndOdds(evenSum, oddSum));
+
+        }
+
+        public static int GetSumOfEvenDigits(string input)
+        {
+            int sum = 0;
+            foreach (var ch in input)
+            {
+                if (ch == '-')
+                {
+                    continue;
+                }
+                if (int.Parse(ch.ToString()) % 2 == 0)
+                {
+                    sum += int.Parse(ch.ToString());
+                }
+            }
+
+            return sum;
+        }
+
+        public static int GetSumOfOddDigits(string input)
+        {
+            int sum = 0;
+            foreach (var ch in input)
+            {
+                if (ch == '-')
+                {
+                    continue;
+                }
+                if (int.Parse(ch.ToString()) % 2 != 0)
+                {
+                    sum += int.Parse(ch.ToString());
+                }
+            }
+
+            return sum;
+        }
+
+        public static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+
+    }
+
+}
+
