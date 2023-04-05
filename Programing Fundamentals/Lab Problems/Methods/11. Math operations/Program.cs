2c3180a32cec64e1d8671c59e34579a4997fbc08
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-05 18:31:55 +0300

    Add MathOperations

---
+++ Programing Fundamentals/Lab Problems/Methods/11. Math operations/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(MathOperator(num1, operation, num2));
+        }
+
+        public static int MathOperator(int num1, string operation, int num2)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return num1 + num2;
+
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+
+            }
+            return 0;
+        }
+    }
+}
