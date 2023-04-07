887ba54688450a7847ddd72cc6a53c643efc685a
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-07 20:11:41 +0300

    Add BigFactorial

---
+++ Programing Fundamentals/Lab Problems/Objects and Classes/02. Big Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            BigInteger currentNum = 1;
+
+            for (int i = num; i > 1; i--)
+            {
+                currentNum *= i;
+            }
+            Console.WriteLine(currentNum);
+        }
+    }
+}
