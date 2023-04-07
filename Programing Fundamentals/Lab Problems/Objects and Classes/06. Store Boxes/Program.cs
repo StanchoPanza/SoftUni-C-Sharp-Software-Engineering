51583445ca57f4f6bea60a68901458be8cd9a8a2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-07 20:15:46 +0300

    Add StoreBoxes

---
+++ Programing Fundamentals/Lab Problems/Objects and Classes/06. Store Boxes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StoreBoxes
+{
+    class Box
+    {
+        public int SerialNumber { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Box> boxes = new List<Box>();
+
+            while (input != "end")
+            {
+                string[] currentItem = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int serialNumber = int.Parse(currentItem[0]);
+                string itemName = currentItem[1];
+                int quantity = int.Parse(currentItem[2]);
+                decimal itemPrice = decimal.Parse(currentItem[3]);
+                decimal totalPrice = quantity * itemPrice;
+
+                Box box = new()
+                {
+                    SerialNumber = serialNumber,
+                    ItemName = itemName,
+                    Quantity = quantity,
+                    ItemPrice = itemPrice,
+                    TotalPrice = totalPrice
+                };
+
+                boxes.Add(box);
+
+                input = Console.ReadLine();
+            }
+            List<Box> orderedList = boxes.OrderByDescending(p => p.TotalPrice).ToList();
+
+            foreach (Box box in orderedList)
+            {
+                Console.WriteLine(box.SerialNumber);
+                Console.WriteLine($"-- {box.ItemName} - ${box.ItemPrice:F2}: {box.Quantity}");
+                Console.WriteLine($"-- ${box.TotalPrice:F2}");
+            }
+        }
+    }
+}
