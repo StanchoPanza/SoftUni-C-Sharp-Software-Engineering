1e00c72e81799b416edbb42e4cf2a90f6879ebca
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:32:39 +0300

    Add RepeatStrings

---
+++ Programing Fundamentals/Lab Problems/Text Processing/02. Repeat Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int k = 0; k < input[i].Length; k++)
+                {
+                    output.Append(input[i]);
+                }
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
