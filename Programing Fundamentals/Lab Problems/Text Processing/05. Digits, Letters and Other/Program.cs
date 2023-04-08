ec1abb08566dd5c871c6e6e2e86e7e3ab91c3dd6
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:34:53 +0300

    Add DigitsLettersandOther

---
+++ Programing Fundamentals/Lab Problems/Text Processing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace DigitsLettersandOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string digits = "";
+            string letters = "";
+
+            StringBuilder leftovers = new StringBuilder();
+
+            foreach (char c in input)
+            {
+                if (Char.IsDigit(c))
+                {
+                    digits += c;
+                }
+                else if (Char.IsLetter(c))
+                {
+                    letters += c;
+                }
+                else
+                {
+                    leftovers.Append(c);
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(leftovers);
+        }
+    }
+}
