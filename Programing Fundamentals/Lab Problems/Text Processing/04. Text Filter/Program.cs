837417d40375bc29cc4bc394119493dd903eb087
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:34:17 +0300

    Add TextFilter

---
+++ Programing Fundamentals/Lab Problems/Text Processing/04. Text Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string textToCheck = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                string asterisks = new string('*', bannedWords[i].Length);
+
+                textToCheck = textToCheck.Replace(bannedWords[i], asterisks);
+            }
+            Console.WriteLine(textToCheck);
+        }
+    }
+}
