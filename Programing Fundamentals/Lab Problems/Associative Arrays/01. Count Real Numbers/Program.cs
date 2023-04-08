3497ea87eaf0da37e0634737930995ffed18e4fc
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:22:34 +0300

    Add CountRealNumbers

---
+++ Programing Fundamentals/Lab Problems/Associative Arrays/01. Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            SortedDictionary<int, int> counted = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (counted.ContainsKey(number))
+                {
+                    counted[number]++;
+                }
+                else
+                {
+                    counted.Add(number, 1);
+                }
+
+            }
+            foreach (var number in counted)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
