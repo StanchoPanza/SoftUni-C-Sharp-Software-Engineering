1fc7b4189bfd1e10f1c1ab05c11fcff0e471357c
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:33:27 +0300

    Add Substring

---
+++ Programing Fundamentals/Lab Problems/Text Processing/03. Substring/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string toRemove = Console.ReadLine();
+            string initString = Console.ReadLine();
+
+
+            while (true)
+            {
+                if (initString.Contains(toRemove))
+                {
+                    int startIndex = initString.IndexOf(toRemove);
+
+                    initString = initString.Remove(startIndex, toRemove.Length);
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(initString);
+        }
+    }
+}
