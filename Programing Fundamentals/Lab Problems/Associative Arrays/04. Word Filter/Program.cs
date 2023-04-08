133f79b262ced0050f176d02031c8df9f375dce0
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:28:15 +0300

    Add WordFilter

---
+++ Programing Fundamentals/Lab Problems/Associative Arrays/04. Word Filter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Where(x => x.Length % 2 == 0).ToArray();
+
+            Console.WriteLine(String.Join(Environment.NewLine, input));
+        }
+    }
+}
