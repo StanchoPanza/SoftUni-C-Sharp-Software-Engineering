41207882ccb7ecc2abfd6401d07a70f665d43559
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-08 16:25:09 +0300

    Add OddOccurrences

---
+++ Programing Fundamentals/Lab Problems/Associative Arrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> counter = new Dictionary<string, int>();
+
+            foreach (var item in input)
+            {
+                string toLower = item.ToLower();
+                if (counter.ContainsKey(toLower))
+                {
+                    counter[toLower]++;
+                }
+                else
+                {
+                    counter.Add(toLower, 1);
+                }
+            }
+            foreach (var item in counter)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+        }
+    }
+}
