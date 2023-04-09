4e517d0818d9dba7f9445faa37fa9eada1b79e1a
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-09 18:57:14 +0300

    Add StrongNumber

---
+++ Programing Fundamentals/Exercise/Basic Syntax, Conditional Statements and Loops/06. Strong number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            long initNum = long.Parse(input);
+            long sum = 0;
+            foreach (var num in input)
+            {
+                long number = int.Parse(num.ToString());
+
+                long factorial = 1;
+
+                for (long i = 1; i <= number; i++)
+                {
+                    factorial *= i;
+                }
+                sum += factorial;
+            }
+
+            if (sum == initNum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
