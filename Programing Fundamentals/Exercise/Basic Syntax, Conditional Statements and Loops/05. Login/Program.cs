8f9a8ca53083361895bff7d62405e01547f13ea2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-09 18:55:32 +0300

    Add Login

---
+++ Programing Fundamentals/Exercise/Basic Syntax, Conditional Statements and Loops/05. Login/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Login
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = Console.ReadLine();
+
+            string reverseUsername = "";
+            int length;
+
+            length = userName.Length - 1;
+
+            while (length >= 0)
+            {
+                reverseUsername = reverseUsername + userName[length];
+                length--;
+            }
+
+            for (int i = 1; i <= 4; i++)
+            {
+                string password = Console.ReadLine();
+                if ((password == reverseUsername))
+                {
+                    Console.WriteLine($"User {userName} logged in.");
+                    return;
+                }
+                else if (password != reverseUsername && i <4)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+            }
+            Console.WriteLine($"User {userName} blocked!");
+            
+        }
+    }
+}
