9c9800530190013999996c0f497758fd6f2961c2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-09 18:47:44 +0300

    Add MatchPhoneNumber

---
+++ Programing Fundamentals/Lab Problems/Regular Expressions/02. Match Phone Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(\+[359]+)([' '|'\-'])[2]\2[0-9]{3}\2[0-9]{4}\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matchCollection = Regex.Matches(input, regex);
+
+            var phoneCollection = matchCollection.Cast<Match>().Select(m => m.Value.Trim()).ToArray();
+
+            Console.Write(String.Join(", ", matchCollection));
+        }
+    }
+}
