fa41641cad6658f3f74afb5884ff8ef4c830a9b2
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-09 18:59:50 +0300

    Add TriangleofNumbers

---
+++ Programing Fundamentals/Exercise/Basic Syntax, Conditional Statements and Loops/08. Triangle of Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TriangleofNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int r = 1; r <= num; r++)
+            {
+                for (int i = 0; i < r; i++)
+                {
+                    Console.Write(r + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
