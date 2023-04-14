18f60dae10cab370e26b1a72c610699ca8cac649
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:08:01 +0300

    Add SumDigits

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/02. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int currentNum = int.Parse(input);
+            int sum = 0;
+            
+
+            for (int i = 1; i <= input.Length; i++)
+            {
+                sum += currentNum % 10;
+                currentNum /= 10;
+            }Console.WriteLine(sum);
+        }
+    }
+}
