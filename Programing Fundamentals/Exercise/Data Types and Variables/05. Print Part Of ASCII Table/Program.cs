a5ecee1a71168c3cc9e356d887ffaa4601a5f28f
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:14:34 +0300

    Add PrintPartoftheASCIITable

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/05. Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5._PrintPartoftheASCIITable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte start = byte.Parse(Console.ReadLine());
+            byte stop = byte.Parse(Console.ReadLine());
+
+            for (int i = start; i <= stop; i++)
+            {
+                char currChar = (char)i;
+                Console.Write($"{currChar} ");
+            }
+        }
+    }
+}
