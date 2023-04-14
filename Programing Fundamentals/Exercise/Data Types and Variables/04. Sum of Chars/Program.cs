d855187c3e8fcf1eed0bb8d0b31f4dff1d8936d8
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:10:41 +0300

    Add SumofChars

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/04. Sum of Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumofChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte lines = byte.Parse(Console.ReadLine());
+
+            int sumOfChars = 0;
+
+            for (int i = 1; i <= lines; i++)
+            {
+                string c = Console.ReadLine();
+                char c2 = char.Parse(c);
+                sumOfChars += c2;
+            }
+            Console.WriteLine($"The sum equals: {sumOfChars}");
+        }
+    }
+}
