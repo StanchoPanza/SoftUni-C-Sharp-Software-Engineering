2b30342d724490976b2aa17c0515eaf62ffc8c93
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:17:41 +0300

    Add SpiceMustFlow

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/09. Spice Must Flow/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger yield = uint.Parse(Console.ReadLine());
+
+            BigInteger spiceMined = 0;
+            BigInteger daysMined = 0;
+
+            if (yield < 100)
+            {
+                Console.WriteLine("0");
+                Console.WriteLine("0");
+                return;
+            }
+            else
+            {
+                while (yield >= 100)
+                {
+                    spiceMined += yield;
+                    spiceMined -= 26;
+                    yield -= 10;
+                    daysMined++;
+                }
+
+            }
+            Console.WriteLine(daysMined);
+            Console.WriteLine(spiceMined - 26);
+
+        }
+    }
+}
