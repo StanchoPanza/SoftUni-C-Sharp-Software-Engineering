931d415f80115f60b74c0c60f4575a0a1350ab30
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:16:51 +0300

    Add BeerKegs

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/08. Beer Kegs/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Numerics;
+
+namespace BeerKegs
+{
+    internal class Program
+    {
+        public static object BigInteger { get; private set; }
+
+        static void Main(string[] args)
+        {
+            int numberOfKegs = int.Parse(Console.ReadLine());
+
+            decimal kegMaxCapacity = 0;
+            string maxCapKeg = "";
+
+            for (int i = 0; i < numberOfKegs; i++)
+            {
+                string kegName = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                decimal kegCapacity = (decimal)(Math.PI * Math.Pow(radius, 2) * height);
+
+                if (kegCapacity > kegMaxCapacity)
+                {
+                    kegMaxCapacity = kegCapacity;
+                    maxCapKeg = kegName;
+                }
+            }
+            Console.WriteLine(maxCapKeg);
+        }
+    }
+}
