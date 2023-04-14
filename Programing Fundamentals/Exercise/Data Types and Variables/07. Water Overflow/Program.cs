679556f521e1ac706f358b2e1f242636445e18d5
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:16:13 +0300

    Add WaterOverflow

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/07. Water Overflow/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumOfLiters = 0;
+            int capacity = 255;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                sumOfLiters += liters;
+               
+                if (sumOfLiters > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sumOfLiters -= liters;
+                }
+            }
+            Console.WriteLine(sumOfLiters);
+        }
+    }
+}
