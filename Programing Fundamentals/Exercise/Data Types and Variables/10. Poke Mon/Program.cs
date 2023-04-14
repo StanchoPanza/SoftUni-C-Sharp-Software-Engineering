f9a4cd70da98c1f915211d595fcc1b546135695c
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:18:42 +0300

    Add PokeMon

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/10. Poke Mon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Poke_Mon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            byte exhaustionFactor = byte.Parse(Console.ReadLine());
+
+            double fiftyPercent = power * 0.5;
+            int pokeCount = 0;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                pokeCount++;
+                if (power == fiftyPercent && exhaustionFactor != 0)
+                {
+                    power /= exhaustionFactor;
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(pokeCount);
+        }
+    }
+}
