9cba433d8258dc74f4b2c655c405521ef145ccc7
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:08:41 +0300

    Add Elevator

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/03. Elevator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peoplesCount = int.Parse(Console.ReadLine());
+            int elevatorCap = int.Parse(Console.ReadLine());
+
+            int courses = 0;
+
+            while (peoplesCount > 0)
+            {
+                peoplesCount -= elevatorCap;
+                courses++;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
