f72ec4b4bebe96688840732a8b104e6a6a413b52
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:03:20 +0300

    Add IntegerOperations

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/01. Integer Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+namespace IntegerOperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            int num4 = int.Parse(Console.ReadLine());
+
+            long sum = (long)num1 + num2;
+            long dividend = sum / (long)num3;
+            long multiplicand = dividend * (long)num4;
+
+            Console.WriteLine(multiplicand);
+
+        }
+    }
+}
