a526b068b34f890ef761428461ee09af2686a414
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-14 15:19:22 +0300

    Add Snowballs

---
+++ Programing Fundamentals/Exercise/Data Types and Variables/11. Snowballs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte ballsCount = byte.Parse(Console.ReadLine());
+
+            BigInteger snowballMaxValue = 0;
+            int snowballSnowMaxValue = 0;
+            int snowballTimeMaxValue = 0;
+            int snowballQualityeMaxValue = 0;
+
+            for (int i = 1; i <= ballsCount; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (snowballValue > snowballMaxValue)
+                {
+                    snowballMaxValue = snowballValue;
+                    snowballSnowMaxValue = snowballSnow;
+                    snowballTimeMaxValue = snowballTime;
+                    snowballQualityeMaxValue = snowballQuality;
+                }
+
+
+            }
+            if (ballsCount == 0)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine($"{snowballSnowMaxValue} : {snowballTimeMaxValue} = {snowballMaxValue} ({snowballQualityeMaxValue})");
+            }
+
+        }
+    }
+}
