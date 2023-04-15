1bc2fa76d725f9bcb4ecf25355711ace65344cd4
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-15 19:24:49 +0300

    Add Zig-ZagArrays

---
+++ Programing Fundamentals/Exercise/Arrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] redNumbers = new int[n];
+            int[] blueNumbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (i == 0 || i % 2 == 0)
+                {
+                    redNumbers[i] = input[0];
+                    blueNumbers[i] = input[1];
+                }
+                else
+                {
+                    redNumbers[i] = input[1];
+                    blueNumbers[i] = input[0];
+                }
+            }
+            Console.WriteLine(String.Join(' ', redNumbers));
+            Console.WriteLine(String.Join(' ', blueNumbers));
+
+
+        }
+    }
+}
