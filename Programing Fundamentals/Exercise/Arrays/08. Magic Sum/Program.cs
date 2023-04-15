161c9d065bf26467ac2a26beee8f25d00a5c9472
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-15 19:29:49 +0300

    Add MagicSum

---
+++ Programing Fundamentals/Exercise/Arrays/08. Magic Sum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace MagicSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int magicNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arr.Length-1; i++)
+            {
+                for (int j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j] + arr[i] == magicNum)
+                    {
+                        Console.WriteLine($"{arr[i]} {arr[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
