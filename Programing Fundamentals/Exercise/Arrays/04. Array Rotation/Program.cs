842be112f34b57da765fd0881f941f468a15ae8f
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-15 19:25:30 +0300

    Add ArrayRotation

---
+++ Programing Fundamentals/Exercise/Arrays/04. Array Rotation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int rotations = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rotations; i++)
+            {
+                int firstToLast = arr[0];
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    arr[j] = arr[j + 1];
+
+                }
+                arr[arr.Length-1] = firstToLast;
+            }
+            Console.WriteLine(String.Join(' ', arr));
+        }
+    }
+}
