eeaa520b05d3b67322534ce47b63c281437250ab
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-15 19:29:05 +0300

    Add MaxSequenceofEqualElements

---
+++ Programing Fundamentals/Exercise/Arrays/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace MaxSequenceofEqualElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int count = 0;
+            int maxCount = 0;
+            string numbers = string.Empty;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        numbers = arr[i].ToString();
+                    }
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            for (int j = 0; j <= maxCount; j++)
+            {
+                Console.Write(numbers + ' ');
+            }
+        }
+    }
+}
