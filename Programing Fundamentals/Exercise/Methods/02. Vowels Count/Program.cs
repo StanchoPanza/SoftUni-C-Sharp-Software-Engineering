875b8b880edc55163cbacfe9968576fdcc98ef00
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:16:38 +0300

    Add VowelsCount

---
+++ Programing Fundamentals/Exercise/Methods/02. Vowels Count/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VowelsCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            VowelsCount(input);
+
+        }
+        static void VowelsCount(string input) 
+        {
+            int vowelsCount = 0;
+            input = input.ToLower();
+
+            char[] vowels = new char[5]
+                {'a', 'e', 'u', 'i', 'o' };
+
+            for (int i = 0; i < vowels.Length; i++) 
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (vowels[i] == input[j])
+                    {
+                        vowelsCount++;
+                    }
+                }
+            }
+            Console.WriteLine(vowelsCount);
+        }
+    }
+}
