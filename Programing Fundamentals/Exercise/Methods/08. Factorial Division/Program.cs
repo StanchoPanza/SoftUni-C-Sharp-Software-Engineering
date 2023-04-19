007ce437ba7e90fb4960bbcc90432fab7376faf8
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:23:22 +0300

    Add FactorialDivision

---
+++ Programing Fundamentals/Exercise/Methods/08. Factorial Division/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FactorialDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal num1 = int.Parse(Console.ReadLine());
+            decimal num2 = int.Parse(Console.ReadLine());
+
+            decimal num1Factorial = NumberFactorial(num1);
+            decimal num2Factorial = NumberFactorial(num2);
+
+            FactorialDivision(num1Factorial, num2Factorial);
+        }
+        static decimal NumberFactorial(decimal num1)
+        {
+            decimal result = num1;
+            for (decimal i = num1 -1; i > 1; i--)
+            {                
+                result *= i;
+            }return result;
+        }
+        static void FactorialDivision(decimal num1, decimal num2)
+        {
+            decimal result = num1 / num2;
+            Console.WriteLine($"{result:F2}");
+        }
+    }
+}
