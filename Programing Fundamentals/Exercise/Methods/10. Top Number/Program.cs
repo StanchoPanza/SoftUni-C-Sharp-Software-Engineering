1dbafc661945c12639f28f1af952fe81fc5217c6
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:33:32 +0300

    Add TopNumber

---
+++ Programing Fundamentals/Exercise/Methods/10. Top Number/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace TopNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string topOfRange = Console.ReadLine();
+
+            TopNumbers(topOfRange);
+        }
+        static void TopNumbers(string num)
+        {
+            int value = int.Parse(num);
+
+            for (int i = 8; i <= value; i++)
+            {
+                int sum = 0;
+                int[] currentNumArray = i.ToString().Select(x => int.Parse(x.ToString())).ToArray();
+                bool thereisOdd = false;
+
+                for (int j = 0; j < currentNumArray.Length; j++)
+                {
+                    
+                    sum += currentNumArray[j];
+
+                    if (currentNumArray[j] % 2 != 0)
+                    {
+                        thereisOdd = true;
+                    }
+                    
+                }
+                if (sum % 8 == 0 && thereisOdd == true)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
