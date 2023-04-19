3ddfd1ae404ff24373f5d8bed25730a1a397caa3
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:20:22 +0300

    Add MiddleCharacters

---
+++ Programing Fundamentals/Exercise/Methods/06. Middle Characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input.Length % 2 == 0)
+            {
+                evenCharacters(input);
+            }
+            else
+            {
+                oddCharacter(input);
+            }
+        }
+        static void evenCharacters(string input)
+        {
+            int result = input.Length / 2;
+            Console.WriteLine($"{input[result - 1]}{input[result]}");
+        }
+        static void oddCharacter(string input)
+        {
+            int result = (input.Length / 2);
+
+            Console.WriteLine(input[result]);
+        }
+
+    }
+}
