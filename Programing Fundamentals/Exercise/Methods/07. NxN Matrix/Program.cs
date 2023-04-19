9b006d947c4d0ba2ac1b7e4c6ae0fb606dca254f
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:21:31 +0300

    Add NxNMatrix

---
+++ Programing Fundamentals/Exercise/Methods/07. NxN Matrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NxNMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            XnXMatrix(n);
+        }
+        static void XnXMatrix(int counter)
+        {
+            for (int i = 1; i <= counter; i++)
+            {
+                for (int j = 1; j <= counter; j++)
+                {
+                    Console.Write(counter + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
