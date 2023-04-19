eb8f843bba833d8e10d48fef9fccdfe1a6649765
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:32:38 +0300

    Add PalindromeIntegers

---
+++ Programing Fundamentals/Exercise/Methods/09. Palindrome Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                ReversInput(input);
+                input = Console.ReadLine();
+            }
+        }static void ReversInput(string input) 
+        {
+            string reversed = String.Empty;
+            char[] charArray = input.ToCharArray();
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+                reversed += charArray[i];
+            }
+            if (reversed == input)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+        }
+
+    }
+}
