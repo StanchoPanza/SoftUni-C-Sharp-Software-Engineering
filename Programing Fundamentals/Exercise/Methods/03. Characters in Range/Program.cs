6520696e90b767238c3702af5cbd04a9b257b5b0
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-19 19:17:15 +0300

    Add CharactersinRange

---
+++ Programing Fundamentals/Exercise/Methods/03. Characters in Range/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CharactersinRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+
+            CharactersinRange(one, two);
+
+
+        }
+        static void CharactersinRange(char a, char b)
+        {
+            if (a < b)
+            {
+                for (int i = a; i < b - 1; i++)
+                {
+                    a++;
+                    Console.Write(a + " ");
+
+                }
+            }
+            else
+            {
+                for (int i = b; i < a - 1; i++)
+                {
+                    b++;
+                    Console.Write(b + " ");
+
+                }
+            }
+            
+        }
+    }
+}
