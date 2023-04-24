abc24555ae204362a01c3b0f06c34a26677c217f
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-24 19:29:25 +0300

    Add Train

---
+++ Programing Fundamentals/Exercise/Lists/01. Train/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int passangersCount = int.Parse(Console.ReadLine());
+                arr[i] = passangersCount;
+                sum += passangersCount;
+            }
+            Console.WriteLine(string.Join(' ', arr));
+            Console.WriteLine(sum);
+        }
+    }
+}
