7a78104e969bb4bf4ce31f199afef0e5e7320d5b
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-24 19:30:50 +0300

    Add HouseParty

---
+++ Programing Fundamentals/Exercise/Lists/03. House Party/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HouseParty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listOfGuests = new List<string>();
+
+            int guestCount = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < guestCount; i++)
+            {
+                List<string> input = Console.ReadLine().Split().ToList();
+
+                if (input[2] == "going!")
+                {
+                    IsGoing(input, listOfGuests);
+                }
+                else
+                {
+                    IsNotGoing(input, listOfGuests);
+                }
+            }
+            foreach (string guests in listOfGuests) 
+            {
+                Console.WriteLine(guests);
+            }
+        }
+
+        static List<string> IsGoing(List<string> input, List<string> output)
+        {
+            if (output.Contains(input[0]))
+            {
+                Console.WriteLine($"{input[0]} is already in the list!");
+                return output;
+            }
+            else
+            {
+                output.Add(input[0]);
+                return output;
+            }
+        }
+        static List<string> IsNotGoing(List<string> input, List<string> output)
+        {
+            if (output.Contains(input[0]))
+            {
+                output.Remove(input[0]);
+                return output;
+            }
+            else
+            {
+                Console.WriteLine($"{input[0]} is not in the list!");
+                return output;
+            }
+        }
+    }
+}
