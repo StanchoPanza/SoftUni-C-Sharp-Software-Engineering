7a1694fd0c0f0b7ed52a414cb5d25a8e037577c5
StanchoPanza <stanislav.stoev@gmail.com>
2023-04-24 20:02:08 +0300

    Add Append Arrays

---
+++ Programing Fundamentals/Exercise/Lists/07. Append Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split("|")
+                .Reverse()
+                .ToArray();
+
+
+            string temp = String.Join(" ", input);
+
+            string[] result = temp.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
