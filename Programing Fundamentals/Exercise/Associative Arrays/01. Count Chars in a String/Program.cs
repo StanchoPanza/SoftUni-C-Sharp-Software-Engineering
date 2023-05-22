a0fa0f2084fff0ac535bc6326ae4bdccb4f97aab
StanchoPanza <stanislav.stoev@gmail.com>
2023-05-22 19:37:56 +0300

    Add CountCharsinAString

---
+++ Programing Fundamentals/Exercise/Associative Arrays/01. Count Chars in a String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountCharsinAString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            input = input.Replace(" ", "");
+            char[] charArray = input.ToCharArray();
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            foreach (char c in charArray)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+            foreach (var symbol in dict)
+            {
+                Console.WriteLine($"{symbol.Key} -> {symbol.Value}");
+            }
+        }
+    }
+}
