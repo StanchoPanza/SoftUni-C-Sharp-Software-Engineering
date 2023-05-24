47a21f31bbc35899918292ef71ff87f72ab96398
StanchoPanza <stanislav.stoev@gmail.com>
2023-05-24 14:49:42 +0300

    Add StudentAcademy

---
+++ Programing Fundamentals/Exercise/Associative Arrays/06. Student Academy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentAcademy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentBook = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!studentBook.ContainsKey(studentName))
+                {
+                    studentBook.Add(studentName, new List<double>());
+                    studentBook[studentName].Add(studentGrade);
+                    continue;
+                }
+                else
+                {
+                    studentBook[studentName].Add(studentGrade);
+                }
+            }
+
+            foreach (string studentName in studentBook.Keys)
+            {
+                double avarageGrade = studentBook[studentName].Average();
+
+                if (avarageGrade >= 4.50)
+                {
+                    Console.WriteLine($"{studentName} -> {avarageGrade:F2}");
+                }
+                else
+                {
+                    studentBook.Remove(studentName);
+                }
+                
+            }
+        }
+    }
+}
