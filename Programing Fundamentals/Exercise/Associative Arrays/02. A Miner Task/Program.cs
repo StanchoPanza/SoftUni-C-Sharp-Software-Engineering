6a92d7c0fcd8cf53f6fa24aecf36f1e415e26789
StanchoPanza <stanislav.stoev@gmail.com>
2023-05-24 14:45:01 +0300

    Add AMinerTask

---
+++ Programing Fundamentals/Exercise/Associative Arrays/02. A Miner Task/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> materials = new Dictionary<string, long>();
+            while (true)
+            {
+                string material = Console.ReadLine();
+                
+                if (material == "stop")
+                {
+                    break;
+                }
+
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (materials.ContainsKey(material))
+                {
+                    materials[material] += quantity;
+                }
+                else
+                {
+                    materials.Add(material, quantity);
+                }
+            }
+            foreach (var kvp in materials)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
